1b14c57e04a8fb81998d0c81a283749762753c4d
frank.96lol@gmail.com <frank.96lol@gmail.com>
2023-02-22 20:56:10 -0500

    server: Fix Mongodb Url

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 //
 const PORT = process.env.PORT || 3000;
 const URL = "http://localhost:" + PORT;
-const URLDB = "mongodb://localhost:27017/ssc";
+const URLDB = "mongodb://rrodnyestrada1:EWo4x8c6Jj3SwCaB@cluster0.r7gu9fw.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = {
     TEST_MODE: process.env.TEST === "true",
