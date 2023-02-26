1cfc74dd637ddac76637f66c73192e93ace308c9
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-02-26 15:17:21 -0500

    client: Add Layout UI Component for 'one page' sections

---
+++ client/ui/layout/layout.js
@@ -0,0 +1,45 @@
+/*
+ * Layout Component
+ */
+let layoutVisible = null;
+
+
+/**
+ * Create a new layout
+ *
+ * @constructor
+ * @param {string} css selector of element
+ */
+class ViewComponent {
+    
+    constructor (selector) {
+        const el = document.querySelector(selector);
+        el.classList.add("layout");
+
+        this.element = el;
+        this.classList = el.classList;
+        if (!layoutVisible) this.show();
+    }
+
+
+    /** 
+     * show layout and hide actual visible layout
+     */
+    show () {
+        if (this.visible) return;
+        this.visible = true;
+        this.classList.add("layout--visible");
+
+        if (layoutVisible) layoutVisible.hide();
+        layoutVisible = this;
+    }
+
+    /** 
+     * hide layout
+     */
+    hide () {
+        if (!this.visible) return;
+        this.visible = false;
+        this.classList.remove("layout--visible");
+    }
+}
