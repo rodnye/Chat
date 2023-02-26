b1c8df3f5b88448f3c3abb8298472ed3dd39dc48
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-02-26 00:48:24 -0500

    client: Add browser console init (eruda)

--- client/router.js
+++ client/router.js
@@ -4,6 +4,10 @@
 
 new Import()
     
+    // start browser console
+    .js("node_modules/eruda/eruda.js")
+    .then(() => eruda.init())
+    
     // main files
     .js("js/main.js")
     .css("css/main.css")
