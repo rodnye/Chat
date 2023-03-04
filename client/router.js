002901b5d41d646fc9a65c75ac2ece92290b71cf
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-03-03 22:53:56 -0500

    client: Add socket.io dependency && Update file paths

--- client/router.js
+++ client/router.js
@@ -9,15 +9,25 @@ new Import("/public")
     
     .css("/node_modules/bootstrap/dist/css/bootstrap-utilities.min.css")
     .css("/node_modules/@fortawesome/fontawesome-free/css/all.min.css")
+    .js("/node_modules/socket.io/client-dist/socket.io.min.js")
     .js("js/animate.js")
     .js("js/storage.js")
+    
+    
+    // main files
+    .js("js/main.js")
+    .css("css/main.css")
+    
+    .js("js/socket/connect.js")
     
     
     // UI components
     .link("ui/layout/layout", "css,js")
     .link("ui/modal/modal", "css,js")
-    .link("ui/input/input", "css")
     .link("ui/button/button", "css")
+    .link("ui/navbar/navbar", "css")
+    .link("ui/fab/fab", "css")
+    .link("ui/input/input", "css")
     .css("ui/utils.css")
     
     
@@ -25,12 +35,8 @@ new Import("/public")
     .link("layouts/auth/auth", "html,css,js")
     .js("layouts/auth/__login.js")
     .js("layouts/auth/__signup.js")
-    .link("layouts/loading__modal/loading", "html,css,js")
-    
-    
-    // main files
-    .js("js/main.js")
-    .css("css/main.css")
+    .link("layouts/main/main", "html,css,js")
+    .link("layouts/loading_modal/loading", "html,css,js")
     
     
     // start app
