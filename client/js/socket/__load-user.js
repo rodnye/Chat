578d90ca02e08401c5f0004d4f76ccd9568592c8
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-03-08 10:02:44 -0500

    client: Add load-user socket listener

---
+++ client/js/socket/__load-user.js
@@ -0,0 +1,23 @@
+
+
+function socketLoadUser (data) {
+    
+    // save user info
+    USER.id = data.user_id;
+    USER.nick = data.nickname;
+    USER.color = data.color;
+    USER.email = data.email;
+    stg.setData("user", USER);
+    
+    
+    // save user contacts 
+    data.contacts.forEach(contact => {
+        CONTACTS[contact.username] = contact;
+    });
+    stg.setData("contacts", CONTACTS);
+    updateContactsList();
+    
+    
+    loading.show("Cargando Chats...");
+    socket.emit("get-room-data", "get");
+}
