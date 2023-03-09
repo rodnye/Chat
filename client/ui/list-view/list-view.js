324800987dc9a9db955e51b003428797bf18bca8
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-03-08 23:02:22 -0500

    client: Add getItemByTitle method and Fix addListener method on [ListView UI Component]

--- client/ui/list-view/list-view.js
+++ client/ui/list-view/list-view.js
@@ -32,6 +32,26 @@ class ListViewComponent {
         return this.items[index];
     }
     
+    
+    /**
+     * Get the item by title
+     *
+     * @param  {string} item title in list
+     * @return {Item_ListViewComponent}
+     */
+    getItemByTitle (title) {
+        const items = this.items;
+        
+        for (let i = 0; i < items.length; i++) {
+            const item = items[i];
+            if (item.title === title) return item;
+        }
+        
+        return null;
+    }
+    
+    
+    
     /**
      * Remove a item 
      * 
@@ -49,14 +69,23 @@ class ListViewComponent {
     }
     
     /**
+     * remove all items
+     */
+    removeAllItems () {
+        while (this.items[0]) this.removeItem(0);
+    }
+    
+    /**
      * Add a list item 
      * 
      * @see Item_ListViewComponent constructor
+     * @return {Item_ListViewComponent}
      */
     addItem(itemData) {
         const item = new Item_ListViewComponent(itemData);
         this.listView.appendChild(item.element);
         this.items.push(item);
+        return item;
     }
 
 
@@ -86,10 +115,20 @@ class ListViewComponent {
      * Add a listener
      */
     addListener (eventName, callback) {
+       
         this.listView.addEventListener(eventName, event => {
-            const item = event.relatedTarget;
-            const itemData = item.dataset;
-            callback();
+            let itemEl = event.target;
+            
+            // find the li element in target
+            let limit = 3;
+            while (!itemEl.dataset.title) {
+               itemEl = itemEl.parentNode;
+               if (!limit) return;
+               limit --;
+            }
+            const item = this.getItemByTitle(itemEl.dataset.title);
+            callback(item);
         });
+        
     }
 }
